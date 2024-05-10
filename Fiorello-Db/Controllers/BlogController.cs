0997ee361fe283dd34b59ba46bb0fea8857ead11
arzusk <arzusk@code.edu.az>
2024-05-10 08:47:11 -0700

    added task

---
+++ Fiorello-Db/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Fiorello_Db.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello_Db.Controllers
+{
+    public class BlogController : Controller
+    {
+        public AppDbContext _context;
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index() => View(await _context.Blogs.Where(m => !m.SoftDeleted).ToListAsync());
+
+    }
+}
